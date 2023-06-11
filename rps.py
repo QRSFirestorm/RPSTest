3c163c5ade2b7a0adf71e66f629be0bbc3d71ee6
GithubPRGOAT <136025717+GithubPRGOAT@users.noreply.github.com>
2023-06-11 12:27:58 -0700

    Create rps.py

---
+++ rps.py
@@ -0,0 +1,18 @@
+import random
+options = ["rock", "paper", "scissors"]
+def rps(option):
+
+    if option == computer:
+        result_message = f"I chose {computer}. It's a tie."
+    elif option == "rock" and computer == "scissors":
+        result_message = f"I chose {computer}. You won."
+    elif option == "paper" and computer == "rock":
+        result_message = f"I chose {computer}. You won."
+    elif option == "scissors" and computer == "paper":
+        result_message = f"I chose {computer}. You won."
+    else:
+        result_message = f"I chose {computer}. You lose!"
+    print("result_message")
+userOption == input("rock, paper or scissors? ")
+rps(userOption)
+  
